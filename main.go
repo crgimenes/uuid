25143e9f417ed84e717a02bfda20518b63d2c8da
Cesar Gimenes <crg@crg.eti.br>
2018-11-22 19:30:02 -0200

    add config

--- main.go
+++ main.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/google/uuid"
+
+	"github.com/crgimenes/goconfig"
 )
 
 func main() {
-	fmt.Print(uuid.New().String())
+	type configFlags struct {
+		CarriageReturn bool   `json:"cr" cfg:"cr" cfgDefault:"false"`
+		LineFeed       bool   `json:"lf" cfg:"lf" cfgDefault:"false"`
+		Uppercase      bool   `json:"u" cfg:"u" cfgDefault:"false"`
+		Armored        bool   `json:"a" cfg:"a" cfgDefault:"false"`
+		ArmorChar      string `json:"ac" cfg:"ac" cfgDefault:"\""`
+		NtoGenerate    int    `json:"n" cfg:"n" cfgDefault:"1"`
+		IDSeparator    string `json:"ids" cfg:"ids" cfgDefault:""`
+	}
+
+	cfg := configFlags{}
+	err := goconfig.Parse(&cfg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cr := ""
+	lf := ""
+	ac := ""
+	if cfg.CarriageReturn {
+		cr = "\r"
+	}
+	if cfg.LineFeed {
+		lf = "\n"
+	}
+	if cfg.Armored {
+		ac = cfg.ArmorChar
+	}
+	for n := 0; n < cfg.NtoGenerate; n++ {
+		id := uuid.New().String()
+		if cfg.Uppercase {
+			id = strings.ToUpper(id)
+		}
+		fmt.Printf("%v%v%v%v%v%v", ac, id, ac, cfg.IDSeparator, cr, lf)
+	}
 }
